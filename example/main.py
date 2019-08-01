4f2f6165b2186a4ea2838c7b85b6914f7f58ecdd
Stuart George <stuart@accentdesign.co.uk>
2019-08-01 16:55:46 +0100

    better example showing use of renditions

--- example/main.py
+++ example/main.py
@@ -1,18 +1,23 @@
+import jinja2
+import os
 import uvicorn
 import sqlalchemy as sa
+import typing
 
 from sqlalchemy import orm
 from starlette.applications import Starlette
 from starlette.endpoints import HTTPEndpoint
-from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 from starlette_core.database import Base, Database, Session
+from starlette_core.templating import Jinja2Templates
 
 from starlette_files.constants import MB
 from starlette_files.fields import ImageAttachment, ImageRenditionAttachment
 from starlette_files.storages import FileSystemStorage
 
 root_directory = "/tmp/starlette"
+dir_path = os.path.dirname(os.path.realpath(__file__))
+templates_path = os.path.join(dir_path, "templates")
 
 
 class MyImage(ImageAttachment):
@@ -31,11 +36,34 @@ class MyImageModel(Base):
     file = sa.Column(MyImage.as_mutable(sa.JSON))
     renditions = orm.relationship("MyImageRenditionModel")
 
+    def get_rendition(self, filter_specs: typing.List[str]) -> "MyImageRenditionModel":
+        rendition = MyImageRenditionModel.query.filter(
+            MyImageRenditionModel.image_id==self.id,
+            MyImageRenditionModel.filter_spec=="-".join(filter_specs),
+            MyImageRenditionModel.file["cache_key"]==self.file.cache_key,
+        ).one_or_none()
+
+        if rendition:
+            return rendition
+
+        rendition = MyImageRenditionModel(
+            image_id=self.id,
+            file=MyImageRendition.create_from(self.file, filter_specs),
+            filter_spec="-".join(filter_specs),
+        )
+
+        session = sa.inspect(self).session
+        session.add(rendition)
+        session.commit()
+
+        return rendition
+
 
 class MyImageRenditionModel(Base):
+    file = sa.Column(MyImageRendition.as_mutable(sa.JSON))
     image_id = sa.Column(sa.Integer, sa.ForeignKey("myimagemodel.id"), nullable=False)
     image = orm.relationship("MyImageModel")
-    file = sa.Column(MyImageRendition.as_mutable(sa.JSON))
+    filter_spec = sa.Column(sa.Text)
 
 
 db = Database("sqlite:///")
@@ -44,50 +72,18 @@ db.create_all()
 app = Starlette(debug=True)
 app.mount("/fs", StaticFiles(directory=root_directory, check_dir=False), name="fs")
 
+templates = Jinja2Templates(loader=jinja2.FileSystemLoader(templates_path))
+
 @app.route("/")
 class Homepage(HTTPEndpoint):
     async def get(self, request):
-        html = """
-        <html>
-        <head></head>
-        <body>
-            <form method="post" enctype="multipart/form-data">
-                <input type="file" name="file" required>
-                <input type="submit" value="Submit">
-            </form>
-        </body>
-        </html>
-        """
-        return HTMLResponse(html)
+        return templates.TemplateResponse("home.html", {"request": request})
 
     async def post(self, request):
         form = await request.form()
 
-        image = MyImageModel(
-            file=MyImage.create_from(form["file"].file, form["file"].filename)
-        )
+        saved_file = MyImage.create_from(form["file"].file, form["file"].filename)
+        image = MyImageModel(file=saved_file)
         image.save()
-
-        rendition = MyImageRenditionModel(
-            file=MyImageRendition.create_from(image.file, ["fill-200x200"]),
-            image=image
-        )
-        rendition.save()
-
-        image_url = request.url_for("fs", path=image.file.path)
-        rendition_url = request.url_for("fs", path=rendition.file.path)
-        html = f"""
-        <html>
-        <head></head>
-        <body>
-            <pre>{image.file}</pre>
-            <img src="{image_url}" style="max-width:100%;">
-            <p>{image.file.locate}</p>
-            <pre>{rendition.file}</pre>
-            <img src="{rendition_url}" style="max-width:100%;">
-            <p>{rendition.file.locate}</p>
-            <a href="/">Again...</a>
-        </body>
-        </html>
-        """
-        return HTMLResponse(html)
+        
+        return templates.TemplateResponse("image.html", {"request": request, "image": image})
