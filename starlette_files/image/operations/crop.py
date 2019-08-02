b492c700ef21dcde3a74d5d84a168e47bc1dfd2a
Stuart George <stuart@accentdesign.co.uk>
2019-08-02 12:22:35 +0100

    added better crop operation

--- starlette_files/image/operations/crop.py
+++ starlette_files/image/operations/crop.py
@@ -1,14 +1,35 @@
+from ..rect import Rect
 from .base import Operation
 
 
 class CropOperation(Operation):
-    def construct(self):
-        pass
+    def construct(self, size=None):
+        if size:
+            left_str, top_str, width_str, height_str = size.split("x")
+            self.left = int(left_str)
+            self.top = int(top_str)
+            self.width = int(width_str)
+            self.height = int(height_str)
 
     def run(self, pillow, attachment, env):
+        image_width, image_height = pillow.size
         focal_point = attachment.focal_point
 
-        if not focal_point:
-            return
+        if hasattr(self, "left"):
+            crop_x = min(self.left, image_width - 1)
+            crop_y = min(self.top, image_height - 1)
 
-        return pillow.crop(focal_point)
+            remaining_after_x = image_width - crop_x
+            remaining_after_y = image_height - crop_y
+
+            max_crop_width = min(self.width, crop_x * 2, remaining_after_x * 2)
+            max_crop_height = min(self.height, crop_y * 2, remaining_after_y * 2)
+
+            rect = Rect.from_point(crop_x, crop_y, max_crop_width, max_crop_height)
+
+            pillow = pillow.crop(rect)
+
+        elif focal_point:
+            pillow = pillow.crop(focal_point)
+
+        return pillow
